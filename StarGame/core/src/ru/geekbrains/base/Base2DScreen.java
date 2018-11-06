2b28d09976786dcdff4dc7011b7ba65484992497
Daniil Gavrilov <dgavrilovdv@gmail.com>
2018-11-07 02:43:18 +0300

    lesson 4 half

--- StarGame/core/src/ru/geekbrains/base/Base2DScreen.java
+++ StarGame/core/src/ru/geekbrains/base/Base2DScreen.java
@@ -21,9 +21,9 @@ public class Base2DScreen implements Screen, InputProcessor {
     private Rect glBounds; // границы проэкции world - gl
 
     protected Matrix4 worldToGl;
-    protected Matrix3 screenToWorld; //для пересчета событийных логов из PX в GL
+    protected Matrix3 screenToWorld;
 
-    private Vector2 touch; //позиция пользовательского нажатия
+    private Vector2 touch;
 
     @Override
     public void show() {
@@ -43,7 +43,7 @@ public class Base2DScreen implements Screen, InputProcessor {
 
     }
 
-    @Override //получение размера экрана
+    @Override
     public void resize(int width, int height) {
         System.out.println("resize w = " + width + " h = " + height);
         screenBounds.setSize(width, height);
@@ -51,11 +51,16 @@ public class Base2DScreen implements Screen, InputProcessor {
         screenBounds.setBottom(0);
 
         float aspect = width / (float) height;
-        worldBounds.setHeight(42f);
-        worldBounds.setWidth(42f * aspect);
+        worldBounds.setHeight(1f);
+        worldBounds.setWidth(1f*aspect);
         MatrixUtils.calcTransitionMatrix(worldToGl, worldBounds, glBounds);
         batch.setProjectionMatrix(worldToGl);
         MatrixUtils.calcTransitionMatrix(screenToWorld, screenBounds, worldBounds);
+        resize(worldBounds);
+    }
+
+    public void resize(Rect worldBounds) {
+
     }
 
     @Override
@@ -119,6 +124,7 @@ public class Base2DScreen implements Screen, InputProcessor {
 
     public boolean touchUp(Vector2 touch, int pointer) {
         System.out.println("touchUp touch.x = " + touch.x + " touch.y = " + touch.y);
+
         return false;
     }
 
@@ -135,7 +141,7 @@ public class Base2DScreen implements Screen, InputProcessor {
 
     @Override
     public boolean scrolled(int amount) {
-        System.out.println("scrolled...");
+        System.out.println("scrolled");
         return false;
     }
 }
--- StarGame/core/src/ru/geekbrains/screen/MenuScreen.java
+++ StarGame/core/src/ru/geekbrains/screen/MenuScreen.java
@@ -3,59 +3,80 @@ package ru.geekbrains.screen;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 import ru.geekbrains.base.Base2DScreen;
+import ru.geekbrains.math.Rect;
+import ru.geekbrains.sprite.Background;
+import ru.geekbrains.sprite.Star;
 
 public class MenuScreen extends Base2DScreen {
 
-    private Texture img;
-    private Vector2 pos;
-    private Vector2 touch;
-    private Vector2 v;
-    private Vector2 buf;
+    private static final int STAR_COUNT = 256;
+
+    private Texture bgTexture;
+    private Background background;
+
+    private TextureAtlas textureAtlas;
+    private Star[] stars;
 
 
     @Override
     public void show() {
         super.show();
-        img = new Texture("badlogic.jpg");
-        pos = new Vector2(0, 0);
-        touch = new Vector2();
-        v = new Vector2();
-        buf = new Vector2();
-
+        bgTexture = new Texture("bg.png");
+        background = new Background(new TextureRegion(bgTexture));
+        textureAtlas = new TextureAtlas("menuAtlas.tpack");
+        stars =new Star[STAR_COUNT];
+        for (int i = 0; i < stars.length; i++) {
+            stars[i] = new Star(textureAtlas);
+        }
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
+        update(delta);
+        draw();
+
+    }
+
+    public void update(float delta) {
+        for (int i = 0; i < stars.length; i++) {
+            stars[i].update(delta);
+        }
+    }
+
+    public void draw() {
         Gdx.gl.glClearColor(0.128f, 0.53f, 0.9f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        buf.set(touch);
-        if(buf.sub(pos).len()>v.len()){
-            pos.add(v);
-        } else {
-            pos.set(touch);
-        }
         batch.begin();
-        batch.draw(img, pos.x, pos.y    , 0.5f, 0.5f);//установка начальной позиции отрисовки
-        // текстуры в системе координа и размера изображения
+        background.draw(batch);
+        for (int i = 0; i < stars.length; i++) {
+            stars[i].draw(batch);
+        }
         batch.end();
+    }
 
+    @Override
+    public void resize(Rect worldBounds) {
+        background.resize(worldBounds);
+        for (int i = 0; i < stars.length; i++) {
+            stars[i].resize(worldBounds);
+        }
     }
 
     @Override
     public void dispose() {
-        img.dispose();
+        bgTexture.dispose();
+        textureAtlas.dispose();
         super.dispose();
     }
 
     @Override
     public boolean touchDown(Vector2 touch, int pointer) {
-        this.touch = touch;
-        v.set(touch.cpy().sub(pos).scl(0.01f));
         return false;
     }
 }
